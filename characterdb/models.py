391d3c3114215a65846ef33d544d395b9fe49078
Thomas Fulmer <tfulmer1@gmail.com>
2017-02-06 15:00:31 -0500

    initial model work

--- characterdb/models.py
+++ characterdb/models.py
@@ -4,3 +4,7 @@ from django.db import models
 
 # Create your models here.
 class Skill(models.Model):
+    skill_name = models.CharField(max_length=200)
+    skill_iq_min = models.IntegerField(default=10)
+    skill_memory = models.IntegerField(default=1)
+    skill_description = models.CharField(max_length=5000)
