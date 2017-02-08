f8e58478c983fcc30e4a1d31b5d870507153c6bb
tfulmer1@gmail.com <tfulmer1@gmail.com>
2017-02-07 21:20:16 -0500

    changes from laptop

--- characterdb/models.py
+++ characterdb/models.py
@@ -4,7 +4,24 @@ from django.db import models
 
 # Create your models here.
 class Skill(models.Model):
-    skill_name = models.CharField(max_length=200)
+    skill_name = models.CharField(primary_key=True,max_length=200)
     skill_iq_min = models.IntegerField(default=10)
     skill_memory = models.IntegerField(default=1)
-    skill_description = models.CharField(max_length=5000)
+    skill_description = models.TextField()
+    non_class_multiplier = models.IntegerField(default=2)
+
+class Spell(models.Model):
+    spell_name = models.CharField(primary_key=True,max_length=200)
+    spell_iq_min = models.IntegerField(default=10)
+    spell_memory = models.IntegerField(default=1)
+    non_class_multiplier = models.IntegerField(default=3)
+    spell_description = models.TextField()
+
+class Item(models.Model):
+
+
+
+class Weapon(models.Model):
+
+
+class
---
+++ setup.sh
@@ -0,0 +1,2 @@
+sudo apt-get update && sudo apt-get install -y python-pip
+sudo pip install Django==1.10.5
