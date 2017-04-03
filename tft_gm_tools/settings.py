beef2f2e7ec64ba4131bf90e8176a48489a75eea
tfulmer1@gmail.com <tfulmer1@gmail.com>
2017-04-02 20:39:09 -0400

    getting more tutorial steps done

--- tft_gm_tools/settings.py
+++ tft_gm_tools/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'diceroll.apps.DicerollConfig',
+    'dicestats.apps.DicestatsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- tft_gm_tools/urls.py
+++ tft_gm_tools/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^diceroll/', include('diceroll.urls')),
+    url(r'^dicestats/', include('dicestats.urls'))
 ]
